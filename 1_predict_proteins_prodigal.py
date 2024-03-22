d0f80daec1c394eb80fcca21a11342d3db22faa8
carolinaamg <47184203+carolinaamg@users.noreply.github.com>
2024-03-22 13:10:30 -0400

    Update 1_predict_proteins_prodigal.py
    small edits :)

--- 1_predict_proteins_prodigal.py
+++ 1_predict_proteins_prodigal.py
@@ -15,7 +15,7 @@ def predict_proteins(input_dir,outfolder, quiet):
 				cmd = "prodigal -i "+ fasta +" -a "+ prot +" -d "+ nucl
 			else:
 				cmd = "prodigal -i "+ fasta +" -a "+ prot +" -d "+ nucl + " -q"
-			print (i)
+			print (cmd)
 			cmd2 = shlex.split(cmd)
 			subprocess.call(cmd2, stdout=open("prodigal.out", "w"), stderr=open("prodigal.err", "w"))
 
@@ -25,13 +25,13 @@ def main(argv=None):
 	args_parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description="INFO:\nThis script will run Prodigal on a set of genome files. Make sure Prodigal is installed.\n\nIMPORTANT:\nYour input files must have an .fna extension.", epilog='*******************************************************************\n\n*******************************************************************\n\nMake sure you cite Prodigal and our book chapter!')
 	args_parser.add_argument('-i', '--input', required=True, help='Input folder where genome files are located with ".fna" extension.')
 	args_parser.add_argument('-o', '--output', required=True, help='Output folder where your output files will go.We suggest you use the same folder as your input to keep your files more organized.')
-	args_parser.add_argument('-q', '--quiet', required=False, default=1, help='Run Prodigal quietly? yes? use 0, no? use 1. Default is 1.')
+	args_parser.add_argument('-q', '--quiet', required=False, default=int(1), help='Run Prodigal quietly? yes? use 0, no? use 1. Default is 1.')
 	args_parser = args_parser.parse_args()
 
 	#Setting up parameters
 	input_dir = args_parser.input
 	outfolder = args_parser.output
-	quiet = args_parser.quiet
+	quiet = int(args_parser.quiet)
 	
 	predict_proteins(input_dir,outfolder, quiet)
 
